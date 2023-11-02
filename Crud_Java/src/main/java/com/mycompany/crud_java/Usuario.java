cb760d661ee5edcd6e81e3dad0571f829a1ae256
Yeison Gil <yeisongil.a9701@gmail.com>
2023-11-02 13:16:08 -0500

    Created Usuario

---
+++ Crud_Java/src/main/java/com/mycompany/crud_java/Usuario.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.crud_java;
+
+/**
+ *
+ * @author yeiso
+ */
+public class Usuario {
+    
+    public String usuario;
+    public String nombre;
+    public String correo;
+    
+    public Usuario(String usuario, String nombre, String correo){
+        this.usuario = usuario;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+    
+}
